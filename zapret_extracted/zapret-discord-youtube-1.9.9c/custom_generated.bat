bee936b90fb6790135fbdc6e7065e2c3fd2938f1
Down37App <mironp61@gmail.com>
2026-06-22 16:35:37 +0300

    clean

--- zapret_extracted/zapret-discord-youtube-1.9.9c/custom_generated.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 > nul
-cd /d "%~dp0"
-echo [*] Запуск подобранного обхода DPI (Синтез #209 (mode=multidisorder, pos=midsld, fool=ts, ttl=1))...
-start "zapret: custom" /min "%~dp0bin/winws.exe" --wf-tcp=80,443 --filter-tcp=80,443 --dpi-desync=multidisorder --dpi-desync-split-pos=midsld --dpi-desync-fooling=ts --dpi-desync-ttl=1
-echo [+] Обход активен.
-pause
